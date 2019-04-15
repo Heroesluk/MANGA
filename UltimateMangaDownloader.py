6be32866aef2b45f200baa38d489033a5d6aa975
Heroesluk <45968650+Heroesluk@users.noreply.github.com>
2019-04-15 18:44:53 +0200

    Add files via upload

--- UltimateMangaDownloader.py
+++ UltimateMangaDownloader.py
@@ -105,9 +105,58 @@ def MangaDownloader():
     MangaDownload(ChapterLinks, ChapterKey)
 
 
+def MangaUpdateCheck():
+    def Manga(wat):
+        list = requests.get(wat)
+        list = list.text
+
+        list = list.splitlines()
+
+        a = 0
+        for i in list:
+            if a == 1:
+                chapter = i  # Creates string with most recent chapter
+                break
+            if i == '<div class="row">':  # Finds if it's line right before line with chapter(always inicialize right before first conditonal
+                a = 1
+
+        val = chapter.find('title')
+        chapter = chapter[val + 7:]  # Strips string from left side html
+
+        val = chapter.find('>')
+        return chapter[:val - 1]  # Strips string from right side html, amd returns it
+
+    MangaList = ['https://manganelo.com/manga/domestic_na_kanojo',
+                 'https://manganelo.com/manga/kaguyasama_wa_kokurasetai_tensaitachi_no_renai_zunousen',
+                 'https://manganelo.com/manga/gotoubun_no_hanayome', 'https://mangakakalot.com/manga/pt918921']
+    ChapterList = []
+    for i in MangaList:
+        ChapterList.append(Manga(i))
+
+    for i in ChapterList:
+        print(i)
+
+while True:
+    print('Choose your poison:  ')
+    print('  1 - MangaDownloader')
+    print('  2 - MangaUpdateCheck')
+    print('  3 - exit')
+    choice = input()
 
 
-MangaDownloader()
+    if choice == 1:
+        MangaDownloader()
+
+    elif choice == 2:
+        MangaUpdateCheck()
+
+    elif choice == 3:
+        break
+
+    print('\n' * 16)
+
+
+
 
 
 
