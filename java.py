66492c23c7f2ff231297cf78662e42b43e7205d5
Heroesluk <45968650+Heroesluk@users.noreply.github.com>
2019-10-14 15:47:22 +0200

    Update java.py

--- java.py
+++ java.py
@@ -1,36 +1,35 @@
 import json
 import re
+import os
 
-x = open('G:\lapy\plik4.json')
-y = json.load(x)
-raw_data = y['sessions'][0]
-print(raw_data)
+def extract(raw_data):
+    pattern = re.compile(
+        r'\'lap\': (\d), \'car\': (\d), \'sectors\': (\[(?:\d+,*\s*)*]), \'time\': ([-\d]\d+), \'cuts\': (\d), \'tyre\': \'([a-zA-Z]+)')
 
-pattern = re.compile(r'\'lap\': (\d), \'car\': (\d), \'sectors\': (\[(?:\d+,*\s*)*]), \'time\': ([-\d]\d+), \'cuts\': (\d), \'tyre\': \'([a-zA-Z]+)')
+    values = (re.findall(pattern, str(raw_data)))
+    print('found', len(values),'laps')
 
-values = (re.findall(pattern, str(raw_data)))
-print(len(values))
+    keys = ['lap_number', 'car', 'sectors', 'time', 'cuts', 'tyre']
+    list_of_valkeys = []
+    for i in values:
+        if i[1] != '0':
+            pass
+        else:
+            list_of_valkeys.append(dict(zip(keys, i)))
 
+    print(len(list_of_valkeys), 'laps out of', len(values), 'were the player ones')
 
-keys = ['lap_number', 'car', 'sectors', 'time', 'cuts', 'tyre']
-list_of_valkeys = []
-for i in values:
-    if i[1]!='0':
-        pass
-    else:
-        list_of_valkeys.append(dict(zip(keys, i)))
+    for i in list_of_valkeys:
+        print(i)
 
-
-
-
-for i in list_of_valkeys:
-    print(i)
+    print('\n')
 
 
-
-
-
-
+for file in os.listdir('G:\lapy'):
+    x = open(os.path.join('G:\lapy',file))
+    y = json.load(x)
+    raw_data = y['sessions'][0]
+    extract(raw_data)
 
 
 
